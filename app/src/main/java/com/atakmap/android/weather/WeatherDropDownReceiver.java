117dfaf8a5d793a02d64f38bfe6bd8e45a70fb35
Helli Kandra <helli@MsiLinux.voo.be>
2022-11-06 15:29:43 +0100

    version code 2022 10 31 - only resources

---
+++ app/src/main/java/com/atakmap/android/weather/WeatherDropDownReceiver.java
@@ -0,0 +1,329 @@
+
+package com.atakmap.android.weather;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.atakmap.coremap.maps.coords.GeoPoint;
+
+import com.atak.plugins.impl.PluginLayoutInflater;
+import com.atakmap.android.maps.MapView;
+import com.atakmap.android.weather.plugin.R;
+import com.atakmap.android.dropdown.DropDown.OnStateListener;
+import com.atakmap.android.dropdown.DropDownReceiver;
+
+import com.atakmap.coremap.log.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class WeatherDropDownReceiver extends DropDownReceiver implements
+        OnStateListener {
+
+    public static final String TAG = WeatherDropDownReceiver.class
+            .getSimpleName();
+
+    public static final String SHOW_PLUGIN = "com.atakmap.android.weather.SHOW_PLUGIN";
+    private final View templateView;
+    private final Context pluginContext;
+    public TextView textView;
+    public ImageButton imageButton;
+    public  TextView textView1;
+    public  TextView textView2;
+    public  TextView textView3;
+    public  TextView textView4;
+    public  TextView textView5;
+
+
+
+    /**************************** CONSTRUCTOR *****************************/
+
+    public WeatherDropDownReceiver(final MapView mapView,
+            final Context context) {
+        super(mapView);
+        this.pluginContext = context;
+
+        // Remember to use the PluginLayoutInflator if you are actually inflating a custom view
+        // In this case, using it is not necessary - but I am putting it here to remind
+        // developers to look at this Inflator
+        templateView = PluginLayoutInflater.inflate(context,
+                R.layout.main_layout, null);
+
+    }
+
+    /**************************** PUBLIC METHODS *****************************/
+
+    public void disposeImpl() {
+    }
+
+    /**************************** INHERITED METHODS *****************************/
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        final String action = intent.getAction();
+        if (action == null)
+            return;
+
+        if (action.equals(SHOW_PLUGIN)) {
+
+            Log.d(TAG, "showing plugin drop down");
+            showDropDown(templateView, HALF_WIDTH, FULL_HEIGHT, FULL_WIDTH,
+                    HALF_HEIGHT, false, this);
+            textView1 = templateView.findViewById(R.id.textView1);
+            textView2 = templateView.findViewById(R.id.textView2);
+            textView3 = templateView.findViewById(R.id.textView3);
+            textView4 = templateView.findViewById(R.id.textView4);
+            //textView5 = templateView.findViewById(R.id.textView5);
+            imageButton = templateView.findViewById(R.id.imageButton);
+
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Double lat = getMapView().getSelfMarker().getPoint().getLatitude();
+                    Double longt = getMapView().getSelfMarker().getPoint().getLongitude();
+                    Float latf = lat.floatValue();
+                    Float longtf = longt.floatValue();
+                    String slat = latf.toString();
+                    String slongt = longtf.toString();
+
+                    String url = "https://api.open-meteo.com/v1/forecast?latitude=" + slat + "&longitude=" + slongt + "&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&windspeed_unit=ms&timezone=auto";
+
+                    new GetURLData().execute(url);
+                  // textView1.setText(url);
+                }
+            });
+            }
+        }
+    private class GetURLData extends AsyncTask <String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            textView1.setText("Wait...");
+        }
+
+        @SuppressLint("SuspiciousIndentation")
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpsURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = " ";
+
+                while ((line = reader.readLine()) != null)
+
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                Double latitude = jsonObject.getDouble("latitude");
+                Double longitude = jsonObject.getDouble("longitude");
+                String time = jsonObject.getJSONObject("daily").getJSONArray("time").getString(0);
+                String code = jsonObject.getJSONObject("daily").getJSONArray("weathercode").getString(0);
+                String tempmax = jsonObject.getJSONObject("daily").getJSONArray("temperature_2m_max").getString(0);
+                String tempmin = jsonObject.getJSONObject("daily").getJSONArray("temperature_2m_min").getString(0);
+                //String precipits = jsonObject.getJSONObject("daily").getJSONArray("precipitation_sum").getString(0);
+                //String precipith = jsonObject.getJSONObject("daily").getJSONArray("precipitation_hours").getString(0);
+
+                textView1.setText("Forecast:  " + time.toString());
+                //textView2.setText(code.toString());
+                if (code == String.valueOf(0)) textView3.setText("Clear Sky".toString());
+
+                if (code == String.valueOf(1)) textView3.setText("Mainly Clear".toString());
+                if (code == String.valueOf(2)) textView3.setText("Partly Cloudy".toString());
+                if (code == String.valueOf(3)) textView3.setText("Overcast".toString());
+
+                if (code == String.valueOf(45)) textView3.setText("Fog".toString());
+                if (code == String.valueOf(48)) textView3.setText("Depositing Rime Fog".toString());
+
+                if (code == String.valueOf(51)) textView3.setText("Drizzle: Light".toString());
+                if (code == String.valueOf(53)) textView3.setText("Drizzle: Moderate".toString());
+                if (code == String.valueOf(55))
+                    textView3.setText("Drizzle: Dense Intensity".toString());
+
+                if (code == String.valueOf(56))
+                    textView3.setText("Freezing Drizzle: Light".toString());
+                if (code == String.valueOf(57))
+                    textView3.setText("Freezing Drizzle: Dense Intensity".toString());
+
+                if (code == String.valueOf(61)) textView3.setText("Rain: Slight".toString());
+                if (code == String.valueOf(63)) textView3.setText("Rain: Moderate".toString());
+                if (code == String.valueOf(65))
+                    textView3.setText("Rain: Heavy Intensity".toString());
+
+                if (code == String.valueOf(66))
+                    textView3.setText("Freezing Rain: Light".toString());
+                if (code == String.valueOf(67))
+                    textView3.setText("Freezing Rain: Heavy Intensity".toString());
+
+                if (code == String.valueOf(71)) textView3.setText("Snow Fall: Slight".toString());
+                if (code == String.valueOf(73)) textView3.setText("Snow Fall: Moderate".toString());
+                if (code == String.valueOf(75))
+                    textView3.setText("Snow Fall: Heavy Intensity".toString());
+
+                if (code == String.valueOf(77)) textView3.setText("Snow Grains".toString());
+
+                if (code == String.valueOf(80))
+                    textView3.setText("Rain Showers: Slight".toString());
+                if (code == String.valueOf(81))
+                    textView3.setText("Rain Showers: Moderate".toString());
+                if (code == String.valueOf(82))
+                    textView3.setText("Rain Showers: Violent".toString());
+
+                if (code == String.valueOf(85))
+                    textView3.setText("Snow showers: Slight".toString());
+                if (code == String.valueOf(86)) textView3.setText("Snow showers: Heavy".toString());
+
+                if (code == String.valueOf(95)) textView3.setText("Thunderstorm".toString());
+
+                textView4.setText(tempmin.toString() + "C" + "/" + tempmax.toString() + "C");
+
+                // if (precipits == String.valueOf(0)) textView5.setText("No precipitation");
+                //else textView5.setText("Precipitation:   " + precipits.toString() + precipith.toString());
+
+
+                String url1 = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude + "&zoom=14&addressdetails=1";
+                new GetGeocodeData().execute(url1);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        private class GetGeocodeData extends AsyncTask<String, String, String> {
+
+            @SuppressLint("SuspiciousIndentation")
+            @Override
+            protected String doInBackground(String... strings) {
+                HttpsURLConnection connection1 = null;
+                BufferedReader reader1 = null;
+
+                try {
+                    URL url1 = new URL(strings[0]);
+                    connection1 = (HttpsURLConnection) url1.openConnection();
+                    connection1.connect();
+
+                    InputStream stream = connection1.getInputStream();
+                    reader1 = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer1 = new StringBuffer();
+                    String line1 = " ";
+
+                    while ((line1 = reader1.readLine()) != null)
+
+                        buffer1.append(line1).append("\n");
+
+                    return buffer1.toString();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection1 != null)
+                        connection1.disconnect();
+                    try {
+                        if (reader1 != null)
+                            reader1.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result1) {
+                super.onPostExecute(result1);
+                try {
+                    JSONObject jsonObject1 = new JSONObject(result1);
+                    //String suburb = jsonObject1.getString("suburb");
+                    //String village = jsonObject1.getString("village");
+                    //String town = jsonObject1.getString("town");
+                    //String city = jsonObject1.getString("city");
+                    //String state_district = jsonObject1.getString("state_district");
+                    //String state = jsonObject1.getString("state");
+                   // textView2.setText(suburb + "," + village + "," + town + "," + city + "," + state_district + "," + state);
+                    String display_name = jsonObject1.getString("display_name");
+                    textView2.setText(display_name);
+               } catch (JSONException e) {
+                   e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+
+
+
+    @Override
+    public void onDropDownSelectionRemoved() {
+    }
+
+    @Override
+    public void onDropDownVisible(boolean v) {
+    }
+
+    @Override
+    public void onDropDownSizeChanged(double width, double height) {
+    }
+
+    @Override
+    public void onDropDownClose() {
+    }
+
+}
---
+++ app/src/main/java/com/atakmap/android/weather/WeatherMapComponent.java
@@ -0,0 +1,43 @@
+
+package com.atakmap.android.weather;
+
+import android.content.Context;
+import android.content.Intent;
+import com.atakmap.android.ipc.AtakBroadcast.DocumentedIntentFilter;
+
+import com.atakmap.android.maps.MapView;
+import com.atakmap.android.dropdown.DropDownMapComponent;
+
+import com.atakmap.coremap.log.Log;
+import com.atakmap.android.weather.plugin.R;
+
+public class WeatherMapComponent extends DropDownMapComponent {
+
+    private static final String TAG = "WeatherMapComponent";
+
+    private Context pluginContext;
+
+    private WeatherDropDownReceiver ddr;
+
+    public void onCreate(final Context context, Intent intent,
+            final MapView view) {
+
+        context.setTheme(R.style.ATAKPluginTheme);
+        super.onCreate(context, intent, view);
+        pluginContext = context;
+
+        ddr = new WeatherDropDownReceiver(
+                view, context);
+
+        Log.d(TAG, "registering the plugin filter");
+        DocumentedIntentFilter ddFilter = new DocumentedIntentFilter();
+        ddFilter.addAction(WeatherDropDownReceiver.SHOW_PLUGIN);
+        registerDropDownReceiver(ddr, ddFilter);
+    }
+
+    @Override
+    protected void onDestroyImpl(Context context, MapView view) {
+        super.onDestroyImpl(context, view);
+    }
+
+}
