2bcfdeedbe420c94248b2a73035db24dfb3fe89c
Helli kandra <helli@Helli>
2023-10-11 14:55:11 +0200

    update Weather plugin to 4.10

--- app/src/main/java/com/atakmap/android/weather/plugin/WeatherTool.java
+++ app/src/main/java/com/atakmap/android/weather/plugin/WeatherTool.java
@@ -1,75 +1,19 @@
-
 package com.atakmap.android.weather.plugin;
 
-import com.atakmap.android.ipc.AtakBroadcast;
-import com.atakmap.android.weather.WeatherDropDownReceiver;
-
-import android.app.Activity;
+// ----- Android API import ----- //;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.ViewGroup;
-import transapps.mapi.MapView;
-import transapps.maps.plugin.tool.Group;
-import transapps.maps.plugin.tool.Tool;
-import transapps.maps.plugin.tool.ToolDescriptor;
 
-public class WeatherTool extends Tool implements ToolDescriptor {
+// ----- ATAK API import ----- //
+import com.atak.plugins.impl.AbstractPluginTool;
 
-    private final Context context;
-
-    public WeatherTool(Context context) {
-        this.context = context;
-    }
-
-    @Override
-    public String getDescription() {
-        return context.getString(R.string.app_name);
-    }
-
-    @Override
-    public Drawable getIcon() {
-        return (context == null) ? null
-                : context.getResources().getDrawable(R.drawable.ic_launcher);
-    }
-
-    @Override
-    public Group[] getGroups() {
-        return new Group[] {
-                Group.GENERAL
-        };
-    }
-
-    @Override
-    public String getShortDescription() {
-        return context.getString(R.string.app_name);
-    }
-
-    @Override
-    public Tool getTool() {
-        return this;
-    }
-
-    @Override
-    public void onActivate(Activity arg0, MapView arg1, ViewGroup arg2,
-            Bundle arg3,
-            ToolCallback arg4) {
-
-        // Hack to close the dropdown that automatically opens when a tool
-        // plugin is activated.
-        if (arg4 != null) {
-            arg4.onToolDeactivated(this);
-        }
-        // Intent to launch the dropdown or tool
-
-        //arg2.setVisibility(ViewGroup.INVISIBLE);
-        Intent i = new Intent(
-                WeatherDropDownReceiver.SHOW_PLUGIN);
-        AtakBroadcast.getInstance().sendBroadcast(i);
-    }
-
-    @Override
-    public void onDeactivate(ToolCallback arg0) {
+public class WeatherTool extends AbstractPluginTool
+{
+    /** ************************* CONSTRUCTOR ************************* **/
+    public WeatherTool (final Context context)
+    {
+        super(context, context.getString(R.string.app_name), context.getString(R.string.app_name),
+                context.getResources().getDrawable(R.drawable.ic_launcher),
+                "com.atakmap.android.weather.SHOW_PLUGIN");
+        PluginNativeLoader.init(context);
     }
 }
